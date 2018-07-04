8e1564863b2398eb54b4c2c662a3228c99943940
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-07-04 22:21:30 +0200

    move imports

--- app/actions/server/webserver/server/routes/api/routes-api.ts
+++ app/actions/server/webserver/server/routes/api/routes-api.ts
@@ -1,7 +1,7 @@
 import { RoutesApiInstance } from "./instance/routes-api-instance";
 import { RoutesApiPassword } from "./password/routes-api-password";
-import { Webserver } from "../../webserver";
 import { RoutesAPIAccount } from "./account/routes-api-account";
+import { Webserver } from "../../webserver";
 
 export class RoutesAPI {
 	public Instance = new RoutesApiInstance(this.server);
