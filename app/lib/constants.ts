8bcf43b92b62c5336a79d3c8023a10fbbc276429
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-07-09 21:56:20 +0200

    comment constants and reduce expire time

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,6 +1,24 @@
+/**
+ * The encryption algorithm used by default
+ */
 export type ENCRYPTION_ALGORITHM = typeof ENCRYPTION_ALGORITHM;
+/**
+ * The encryption algorithm used by default
+ */
 export const ENCRYPTION_ALGORITHM: 'aes-256-ctr' = 'aes-256-ctr';
+/**
+ * The length of the key used to reset an account
+ */
 export const RESET_KEY_LENGTH = 512;
+/**
+ * The current version of this server
+ */
 export const VERSION = '0.1.0';
+/**
+ * The URL to the database that should be used for testing
+ */
 export const TEST_DATABASE = 'mongodb://127.0.0.1:27017/__pwmanager_test_db';
-export const EXPIRE_TIME = 1000 * 60 * 20;
+/**
+ * The time in which an auth token expires (15 mins + 3 mins grace period)
+ */
+export const EXPIRE_TIME = 1000 * 60 * 18;
