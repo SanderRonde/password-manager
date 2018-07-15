a5fdb12f9c552081e13c2f971e901909db4ded45
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-07-15 22:20:22 +0200

    remove tasks that don't exist anymore

--- gulpfile.js
+++ gulpfile.js
@@ -15,18 +15,17 @@ function genTask(description, toRun) {
 /* Compilation */
 (() => {
 	gulp.task('compile', genTask('Compiles the typescript',
-		gulp.series('updateTsIdMaps', gulp.parallel(
-			function compileApp() {
-				return new Promise((resolve, reject) => {
-					const project = ts.createProject('tsconfig.json');
-					const proj =  project.src().pipe(project());
-					proj.once('error', () => { 
-						reject('Error(s) thrown during compilation');
-					});
-					proj.js.pipe(gulp.dest('./')).once('end', () => {
-						resolve(null);
-					});
+		function compileApp() {
+			return new Promise((resolve, reject) => {
+				const project = ts.createProject('tsconfig.json');
+				const proj =  project.src().pipe(project());
+				proj.once('error', () => { 
+					reject('Error(s) thrown during compilation');
 				});
-			}
-		))));
+				proj.js.pipe(gulp.dest('./')).once('end', () => {
+					resolve(null);
+				});
+			});
+		}
+	));
 })();
