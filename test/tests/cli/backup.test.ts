dc8ddf3454aa0951320d3c67754e2cd624c1bc50
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-07-15 22:10:16 +0200

    add test combining backupping and restoring

--- test/tests/cli/backup.test.ts
+++ test/tests/cli/backup.test.ts
@@ -1,6 +1,13 @@
+import { genTempDatabase, captureURIs, captureCreatedFiles } from '../../lib/util';
+import { genRandomString } from '../../../app/lib/util';
 import { ProcRunner } from '../../lib/procrunner';
+import { genDBWithPW, clearDB, getDB, hasCreatedDBWithPW } from '../../lib/db';
+import path = require('path');
 import { test } from 'ava';
+import fs = require('fs');
 
+const uris = captureURIs(test);
+const files = captureCreatedFiles(test);
 test('print an error when no command is passed', async t => {
 	const proc = new ProcRunner(t, ['backup']);
 	proc.expectWrite();
@@ -18,4 +25,59 @@ test('print an error when a non-command is used', async t => {
 
 	await proc.run();
 	proc.check();
+});
+test('a backupped file can be used to restore', async t => {
+	const dumpName = genRandomString(10);
+	const dumpPath = path.join(__dirname, `../../../out/mongodump${dumpName}.dump`);
+	const uri = await genTempDatabase(t);
+	uris.push(uri);
+	const dbpw = await genDBWithPW(uri);
+	await (async () => {
+		files.push(dumpPath);
+		const proc = new ProcRunner(t, [
+			'backup',
+			'local',
+			'-d', uri,
+			'-o', dumpPath
+		]);
+		proc.expectWrite('Dumping...');
+		proc.expectWrite('Writing file...');
+		proc.expectWrite('Done writing file');
+		proc.expectExit(0);
+
+		await proc.run();
+		proc.check();
+
+		const exists = await new Promise<boolean>((resolve) => {
+			fs.exists(dumpPath, (exists) => {
+				resolve(exists);
+			});
+		});
+		t.true(exists, 'dump file exists');
+	})();
+	await (async () => {
+		await clearDB(uri);
+
+		const { db, done } = await getDB(uri);
+		t.is((await db.collection('meta').find().toArray()).length, 0,
+			'meta collection is empty');
+		done();
+
+		const proc = new ProcRunner(t, [
+			'backup',
+			'load',
+			'-i', dumpPath,
+			'-d', uri
+		]);
+		proc.expectWrite('Reading file...');
+		proc.expectWrite('Restoring...');
+		proc.expectWrite('Done!');
+		proc.expectExit(0);
+
+		await proc.run();
+		proc.check();
+
+		//Check if the database was actually created
+		t.true(await hasCreatedDBWithPW(dbpw, uri));
+	})();
 });
