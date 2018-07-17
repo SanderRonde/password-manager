61f907d290656c4444f1b888a793170437a27b2f
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-07-18 00:18:15 +0200

    add test for wrong password

--- test/tests/server/api/instance/register.test.ts
+++ test/tests/server/api/instance/register.test.ts
@@ -1,6 +1,7 @@
 import { captureURIs, genUserAndDb, createServer, doAPIRequest, testParams } from '../../../../lib/util';
 import { hash, pad, genRSAKeyPair, decryptWithPrivateKey, ERRS } from '../../../../../app/lib/crypto';
 import { DEFAULT_EMAIL } from '../../../../lib/consts';
+import { API_ERRS } from '../../../../../app/api';
 import { getDB } from '../../../../lib/db';
 import mongo = require('mongodb');
 import { test } from 'ava';
@@ -48,4 +49,26 @@ test('instance can be created', async t => {
 	t.truthy(instance, 'instance was created and ID is correct');
 
 	t.is(typeof server_key, 'string', 'type of serverkey is string');
+});
+test('fails if password is wrong', async t => {
+	const config = await genUserAndDb(t);
+	const server = await createServer(config);
+	const { http, userpw, uri } = config;
+	uris.push(uri);
+
+	const keyPair = genRSAKeyPair();
+	const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/register', {
+		email: DEFAULT_EMAIL,
+		password: hash(pad(userpw + 'wrongpw', 'masterpwverify')),
+		public_key: keyPair.publicKey
+	}));
+
+	server.kill();
+
+	t.false(response.success, 'API call failed');
+	if (response.success) {
+		return;
+	}
+	t.is(response.ERR, API_ERRS.INVALID_CREDENTIALS,
+		'got invalid credentials errors');
 });
