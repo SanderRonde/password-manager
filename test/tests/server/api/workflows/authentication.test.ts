fa5b24b9aeec1125e5b8ed0b6f80aa6ef6a9e04f
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-07-18 04:47:05 +0200

    add workflow tests, combining all previous tests

---
+++ test/tests/server/api/workflows/authentication.test.ts
@@ -0,0 +1,296 @@
+import { genRSAKeyPair, hash, pad, decryptWithPrivateKey, ERRS, encryptWithPublicKey } from '../../../../../app/lib/crypto';
+import { genUserAndDb, createServer, captureURIs, doAPIRequest } from '../../../../lib/util';
+import { genRandomString } from '../../../../../app/lib/util';
+import { DEFAULT_EMAIL } from '../../../../lib/consts';
+import { getDB } from '../../../../lib/db';
+import mongo = require('mongodb');
+import { test } from 'ava';
+
+const uris = captureURIs(test);
+test('can log in after registering instance', async t => {
+	const config = await genUserAndDb(t);
+	const server = await createServer(config);
+	const { 
+		http, 
+		userpw, 
+		uri, 
+	} = config;
+	uris.push(uri);
+
+	const {
+		instance_id, clientPrivateKey, serverPublicKey
+	} = await (async  () => {
+		const keyPair = genRSAKeyPair();
+		const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/register', {
+			email: DEFAULT_EMAIL,
+			password: hash(pad(userpw, 'masterpwverify')),
+			public_key: keyPair.publicKey
+		}));
+
+		t.true(response.success, 'API call succeeded');
+		if (!response.success) {
+			return {};
+		}
+		const instance_id = decryptWithPrivateKey(response.data.id, keyPair.privateKey);
+		const server_key = decryptWithPrivateKey(response.data.server_key, 
+			keyPair.privateKey)
+		
+		t.not(instance_id, ERRS.INVALID_DECRYPT, 'decryption was not successful');
+		t.not(server_key, ERRS.INVALID_DECRYPT, 'decryption was not successful');
+		if (instance_id === ERRS.INVALID_DECRYPT || server_key === ERRS.INVALID_DECRYPT) {
+			return {};
+		}
+
+		const { db, done } = await getDB(uri);
+		const instance = await db.collection('instances').findOne({
+			_id: new mongo.ObjectId(instance_id)
+		});
+		done();
+		t.truthy(instance, 'instance was created and ID is correct');
+
+		t.is(typeof server_key, 'string', 'type of serverkey is string');
+		return {
+			instance_id: instance_id,
+			clientPrivateKey: keyPair.privateKey,
+			serverPublicKey: server_key
+		}
+	})();
+	await (async () => {
+		const challenge = genRandomString(25);
+		const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/login', {
+			instance_id: instance_id!,
+			challenge: encryptWithPublicKey(challenge, serverPublicKey!),
+			password_hash: hash(pad(userpw, 'masterpwverify'))
+		}));
+
+		server.kill();
+
+		t.true(response.success, 'API call succeeded');
+		if (!response.success) {
+			return;
+		}
+		const data = response.data;
+		t.false(data.twofactor_required, 'further authentication is not required');
+		if (data.twofactor_required === true) {
+			return;
+		}
+		const token = decryptWithPrivateKey(data.auth_token, clientPrivateKey!);
+		t.not(token, ERRS.INVALID_DECRYPT, 'is not invalid decrypt');
+		t.is(typeof token, 'string', 'token is a string');
+
+		t.is(data.challenge, challenge, 'challenge matches');
+	})();
+});
+test('can log out after logging in', async t => {
+	const config = await genUserAndDb(t, {
+		twofactor_enabled: false
+	});
+	const server = await createServer(config);
+	const { 
+		http, 
+		userpw, 
+		uri, 
+		server_public_key, 
+		instance_id, 
+		instance_private_key
+	} = config;
+	uris.push(uri);
+
+	const token = await (async () => {	
+		const challenge = genRandomString(25);
+		const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/login', {
+			instance_id: instance_id.toHexString(),
+			challenge: encryptWithPublicKey(challenge, server_public_key),
+			password_hash: hash(pad(userpw, 'masterpwverify'))
+		}));
+		
+		t.true(response.success, 'API call succeeded');
+		if (!response.success) {
+			return;
+		}
+		const data = response.data;
+		t.false(data.twofactor_required, 'further authentication is not required');
+		if (data.twofactor_required === true) {
+			return;
+		}
+		const token = decryptWithPrivateKey(data.auth_token, instance_private_key);
+		t.not(token, ERRS.INVALID_DECRYPT, 'is not invalid decrypt');
+		if (token === ERRS.INVALID_DECRYPT) {
+			return;
+		}
+		t.is(typeof token, 'string', 'token is a string');
+	
+		t.is(data.challenge, challenge, 'challenge matches');
+		return token;
+	})();
+	await (async () => {	
+		const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/logout', {
+			instance_id: instance_id.toHexString(),
+			token: token!
+		}));
+	
+		server.kill();
+	
+		t.true(response.success, 'API call succeeded')
+	})();
+});
+test('can log in and extend key', async t => {
+	const config = await genUserAndDb(t, {
+		twofactor_enabled: false
+	});
+	const server = await createServer(config);
+	const { 
+		http, 
+		userpw, 
+		uri, 
+		server_public_key, 
+		instance_id, 
+		instance_private_key
+	} = config;
+	uris.push(uri);
+
+	const token = await (async () => {	
+		const challenge = genRandomString(25);
+		const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/login', {
+			instance_id: instance_id.toHexString(),
+			challenge: encryptWithPublicKey(challenge, server_public_key),
+			password_hash: hash(pad(userpw, 'masterpwverify'))
+		}));
+		
+		t.true(response.success, 'API call succeeded');
+		if (!response.success) {
+			return;
+		}
+		const data = response.data;
+		t.false(data.twofactor_required, 'further authentication is not required');
+		if (data.twofactor_required === true) {
+			return;
+		}
+		const token = decryptWithPrivateKey(data.auth_token, instance_private_key);
+		t.not(token, ERRS.INVALID_DECRYPT, 'is not invalid decrypt');
+		if (token === ERRS.INVALID_DECRYPT) {
+			return;
+		}
+		t.is(typeof token, 'string', 'token is a string');
+	
+		t.is(data.challenge, challenge, 'challenge matches');
+		return token;
+	})();
+	await (async () => {	
+		const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/extend_key', {
+			instance_id: instance_id.toHexString(),
+			oldToken: token!
+		}));
+	
+		server.kill();
+	
+		t.true(response.success, 'API call succeeded')
+		if (!response.success) {
+			return;
+		}
+		const data = response.data;
+		t.is(typeof data.auth_token, 'string', 'auth token was passed');
+	})();
+});
+test('can register an instance, log in, extend key and log out', async t => {
+	const config = await genUserAndDb(t);
+	const server = await createServer(config);
+	const { 
+		http, 
+		userpw, 
+		uri, 
+	} = config;
+	uris.push(uri);
+
+	const { instanceId, clientPrivateKey, serverPublicKey } = await (async  () => {
+		const keyPair = genRSAKeyPair();
+		const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/register', {
+			email: DEFAULT_EMAIL,
+			password: hash(pad(userpw, 'masterpwverify')),
+			public_key: keyPair.publicKey
+		}));
+
+		t.true(response.success, 'API call succeeded');
+		if (!response.success) {
+			return {};
+		}
+		const instance_id = decryptWithPrivateKey(response.data.id, keyPair.privateKey);
+		const server_key = decryptWithPrivateKey(response.data.server_key, 
+			keyPair.privateKey)
+		
+		t.not(instance_id, ERRS.INVALID_DECRYPT, 'decryption was not successful');
+		t.not(server_key, ERRS.INVALID_DECRYPT, 'decryption was not successful');
+		if (instance_id === ERRS.INVALID_DECRYPT || server_key === ERRS.INVALID_DECRYPT) {
+			return {};
+		}
+
+		const { db, done } = await getDB(uri);
+		const instance = await db.collection('instances').findOne({
+			_id: new mongo.ObjectId(instance_id)
+		});
+		done();
+		t.truthy(instance, 'instance was created and ID is correct');
+
+		t.is(typeof server_key, 'string', 'type of serverkey is string');
+		return {
+			instanceId: instance_id,
+			clientPrivateKey: keyPair.privateKey,
+			serverPublicKey: server_key
+		}
+	})();
+	const token = await (async () => {
+		const challenge = genRandomString(25);
+		const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/login', {
+			instance_id: instanceId!,
+			challenge: encryptWithPublicKey(challenge, serverPublicKey!),
+			password_hash: hash(pad(userpw, 'masterpwverify'))
+		}));
+
+		t.true(response.success, 'API call succeeded');
+		if (!response.success) {
+			return;
+		}
+		const data = response.data;
+		t.false(data.twofactor_required, 'further authentication is not required');
+		if (data.twofactor_required === true) {
+			return;
+		}
+		const token = decryptWithPrivateKey(data.auth_token, clientPrivateKey!);
+		t.not(token, ERRS.INVALID_DECRYPT, 'is not invalid decrypt');
+		if (token === ERRS.INVALID_DECRYPT) {
+			return;
+		}
+		t.is(typeof token, 'string', 'token is a string');
+
+		t.is(data.challenge, challenge, 'challenge matches');
+		return token;
+	})();
+	const newToken = await (async () => {	
+		const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/extend_key', {
+			instance_id: instanceId!,
+			oldToken: token!
+		}));
+	
+		t.true(response.success, 'API call succeeded')
+		if (!response.success) {
+			return;
+		}
+		t.is(typeof response.data.auth_token, 'string', 'auth token was passed');
+		const decrypted = decryptWithPrivateKey(response.data.auth_token, clientPrivateKey!);
+		t.not(decrypted, ERRS.INVALID_DECRYPT, 'is not an invalid decrypt');
+		if (decrypted === ERRS.INVALID_DECRYPT) {
+			return;
+		}
+		return decrypted;
+	})();
+	await (async () => {	
+		const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/logout', {
+			instance_id: instanceId!,
+			token: newToken!
+		}));
+	
+		server.kill();
+	
+		t.true(response.success, 'API call succeeded')
+	})();
+});
