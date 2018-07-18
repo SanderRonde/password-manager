522fdd9f61de6bd6d7f1898337a1d9b94e630833
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-07-18 02:10:44 +0200

    add login route tests

---
+++ test/tests/server/api/instance/login.test.ts
@@ -0,0 +1,93 @@
+import { captureURIs, genUserAndDb, createServer, doAPIRequest, testParams } from '../../../../lib/util';
+import { hash, pad, decryptWithPrivateKey, ERRS, encryptWithPublicKey } from '../../../../../app/lib/crypto';
+import { genRandomString } from '../../../../../app/lib/util';
+import speakeasy = require('speakeasy');
+import { test } from 'ava';
+
+const uris = captureURIs(test);
+testParams(test, uris, '/api/instance/login', {
+	instance_id: 'string',
+	challenge: 'string',
+	password_hash: 'string'
+}, {}, {}, {});
+test('login token can be generated when 2FA is disabled', async t => {
+	const config = await genUserAndDb(t, {
+		twofactor_enabled: false
+	});
+	const server = await createServer(config);
+	const { 
+		http, 
+		userpw, 
+		uri, 
+		server_public_key, 
+		instance_id, 
+		instance_private_key
+	} = config;
+	uris.push(uri);
+
+	const challenge = genRandomString(25);
+	const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/login', {
+		instance_id: instance_id.toHexString(),
+		challenge: encryptWithPublicKey(challenge, server_public_key),
+		password_hash: hash(pad(userpw, 'masterpwverify'))
+	}));
+
+	server.kill();
+
+	t.true(response.success, 'API call succeeded');
+	if (!response.success) {
+		return;
+	}
+	const data = response.data;
+	t.false(data.twofactor_required, 'further authentication is not required');
+	if (data.twofactor_required === true) {
+		return;
+	}
+	const token = decryptWithPrivateKey(data.auth_token, instance_private_key);
+	t.not(token, ERRS.INVALID_DECRYPT, 'is not invalid decrypt');
+	t.is(typeof token, 'string', 'token is a string');
+
+	t.is(data.challenge, challenge, 'challenge matches');
+});
+test('login token can be generated when 2FA is enabled', async t => {
+	const config = await genUserAndDb(t, {
+		twofactor_enabled: true,
+		twofactor_token: speakeasy.generateSecret({
+			name: 'Password Manager'
+		}).base32
+	});
+	const server = await createServer(config);
+	const { 
+		http, 
+		userpw, 
+		uri, 
+		server_public_key, 
+		instance_id, 
+		instance_private_key
+	} = config;
+	uris.push(uri);
+
+	const challenge = genRandomString(25);
+	const response = JSON.parse(await doAPIRequest({ port: http }, '/api/instance/login', {
+		instance_id: instance_id.toHexString(),
+		challenge: encryptWithPublicKey(challenge, server_public_key),
+		password_hash: hash(pad(userpw, 'masterpwverify'))
+	}));
+
+	server.kill();
+
+	t.true(response.success, 'API call succeeded');
+	if (!response.success) {
+		return;
+	}
+	const data = response.data;
+	t.true(data.twofactor_required, 'further authentication is required');
+	if (data.twofactor_required === false) {
+		return;
+	}
+	const token = decryptWithPrivateKey(data.twofactor_auth_token, instance_private_key);
+	t.not(token, ERRS.INVALID_DECRYPT, 'is not invalid decrypt');
+	t.is(typeof token, 'string', 'token is a string');
+
+	t.is(data.challenge, challenge, 'challenge matches');
+});
