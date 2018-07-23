9786e5e5ee383219326c47603bf4820a6c427b67
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-07-23 21:12:48 +0200

    add tsconfig for client side code as well

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,8 @@ function genTask(description, toRun) {
 
 /* Compilation */
 (() => {
-	gulp.task('compile', genTask('Compiles the typescript for /server',
-		function compileApp() {
+	gulp.task('compileServer', genTask('Compiles the typescript for /server',
+		function compileServer() {
 			return new Promise((resolve, reject) => {
 				// @ts-ignore
 				const project = ts.createProject('./server/tsconfig.json');
@@ -24,6 +24,24 @@ function genTask(description, toRun) {
 					reject('Error(s) thrown during compilation');
 				});
 				proj.js.pipe(gulp.dest('./server/')).once('end', () => {
+					resolve(null);
+				});
+			});
+		}
+	));
+
+	gulp.task('compileClientDashboard', genTask('Compiles the typescript for the dashboard',
+		function compileClientDashboard() {
+			return new Promise((resolve, reject) => {
+				// @ts-ignore
+				const dir = './server/app/actions/server/webserver/client/src';
+				const project = 
+					ts.createProject(`${dir}/tsconfig.json`);
+				const proj = project.src().pipe(project());
+				proj.once('error', () => { 
+					reject('Error(s) thrown during compilation');
+				});
+				proj.js.pipe(gulp.dest(`${dir}/`)).once('end', () => {
 					resolve(null);
 				});
 			});
