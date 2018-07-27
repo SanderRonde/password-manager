d0cebb729040038ccc8b8c2ad5eb2e837474d8b7
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-07-27 04:02:50 +0200

    fix typing

--- shared/lib/classnames.ts
+++ shared/lib/classnames.ts
@@ -2,25 +2,30 @@
 
 const hasOwn = {}.hasOwnProperty;
 
-export function classNames () {
+type ClassNamesArg = string|{
+	[key: string]: any;
+}|string[]|{
+	[key: string]: any;
+}[];
+export function classNames(...args: ClassNamesArg[]) {
 	var classes = [];
 
-	for (var i = 0; i < arguments.length; i++) {
-		var arg = arguments[i];
+	for (const arg of args) {
 		if (!arg) continue;
 
-		var argType = typeof arg;
-
-		if (argType === 'string' || argType === 'number') {
+		if (typeof arg === 'string' || typeof arg === 'number') {
 			classes.push(arg);
 		} else if (Array.isArray(arg) && arg.length) {
 			var inner = classNames.apply(null, arg);
 			if (inner) {
 				classes.push(inner);
 			}
-		} else if (argType === 'object') {
-			for (var key in arg) {
-				if (hasOwn.call(arg, key) && arg[key]) {
+		} else if (typeof arg === 'object') {
+			const objArg = arg as {
+				[key: string]: any;
+			};
+			for (var key in objArg) {
+				if (hasOwn.call(objArg, key) && objArg[key]) {
 					classes.push(key);
 				}
 			}
