4e575bfcce0c87031721d735e87931d00fcb8bec
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-07-29 04:54:46 +0200

    add favicon to html

--- server/app/actions/server/webserver/client/src/html.ts
+++ server/app/actions/server/webserver/client/src/html.ts
@@ -10,6 +10,7 @@ export function preAppHTML({
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
 		${development ? '' : `
 		<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src 'self'; script-src 'self' https://unpkg.com; style-src 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com">
 		`}
