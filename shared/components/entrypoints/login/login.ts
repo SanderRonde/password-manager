afe7987033fbfdd74fa8f44d89e2fca8df776fee
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-02 04:13:58 +0200

    add other inputs

--- shared/components/entrypoints/login/login.ts
+++ shared/components/entrypoints/login/login.ts
@@ -540,8 +540,18 @@ export class Login extends WebComponent {
 							<material-input id="emailInput" name="email"
 								type="email" title="Account's email"
 								error="Please enter a valid email address"
-								autoComplete="username" fill
-								autoFocus label="Email" />
+								autoComplete="username" fill required
+								autoFocus label="Email"></material-input>
+							<material-input id="passwordInput" name="password"
+								type="password" title="Account password"
+								error="Please enter a password"
+								autoComplete="password" fill required
+								autoFocus label="Password"></material-input>
+							<material-input id="twofactorInput" name="twofactor"
+								type="tel" autoComplete="off" fill
+								pattern="\d{6}" error="Enter a 6-digit code"
+								title="Twofactor authentication token (if enabled for the account)"
+								autoFocus label="2FA Token (if enabled)"></material-input>
 						</div>
 					</vertical-centerer>
 				</horizontal-centerer>
