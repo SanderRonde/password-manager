1a36989aed14b7e948cdf8de78b28618bc7fd2a4
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-02 04:23:47 +0200

    add descriptions to theme

--- shared/components/theming/theme.ts
+++ shared/components/theming/theme.ts
@@ -1,17 +1,67 @@
 export const theme = {
+	/**
+	 * The primary color
+	 */
 	primary: {
+		/**
+		 * The main primary color
+		 * dark blue-grey
+		 */
 		main: '#455A64',
+		/**
+		 * The lighter version of the primary color
+		 * light blue-grey
+		 */
 		light: '#718792',
+		/**
+		 * The darker version of the primary color
+		 * dark blue-grey
+		 */
 		dark: '#1C313A'
 	},
+	/**
+	 * The accent/secondary color
+	 */
 	accent: {
+		/**
+		 * The main accent color
+		 * dark-ish purple
+		 */
 		main: '#7B1FA2',
+		/**
+		 * The lighter version of the accent color
+		 * lighter purple
+		 */
 		light: '#AE52D4',
+		/**
+		 * The darker version of the accent color
+		 * dark purple
+		 */
 		dark: '#4A0072'
 	},
+	/**
+	 * The color of error messages
+	 * red with a hint of orange
+	 */
 	error: '#F44336',
+	/**
+	 * The greyest possible text that still fits the contrast spec on a white background
+	 * black
+	 */
 	minTextOnWhite: '#0000008C',
+	/**
+	 * Regular black text on a white background
+	 * black
+	 */
 	textOnWhite: '#000000',
+	/**
+	 * Regular white text on a black background
+	 * white
+	 */
 	textOnBlack: '#FFFFFF',
+	/**
+	 * The default page background color
+	 * white
+	 */
 	background: '#FFFFFF'
 }
