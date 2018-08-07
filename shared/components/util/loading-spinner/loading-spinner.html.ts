e263267d6fa3333a934a50311248d20b41249f4d
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-07 04:31:14 +0200

    insert css in loading-spinner

--- shared/components/util/loading-spinner/loading-spinner.html.ts
+++ shared/components/util/loading-spinner/loading-spinner.html.ts
@@ -3,6 +3,7 @@ import { html } from "lit-html";
 
 export function LoadingSpinnerHTML(this: LoadingSpinner) {
 	return html`
+		${this.css}
 		<div id="container" class="mdl-spinner mdl-js-spinner is-active"></div>
 	`;
 }
