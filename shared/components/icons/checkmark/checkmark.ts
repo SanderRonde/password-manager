826d41d83b31ea7ba87e3b8b9384b9d80f759104
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-07 04:30:55 +0200

    add variable widths for icons

--- shared/components/icons/checkmark/checkmark.ts
+++ shared/components/icons/checkmark/checkmark.ts
@@ -1,7 +1,14 @@
-import { html } from "lit-html";
+import { html, TemplateResult } from "lit-html";
 
-export const Checkmark = html`
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-    <path fill="none" d="M0 0h24v24H0z"/>
-    <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/>
-</svg>`;
+export const Checkmark = CheckmarkSize();
+
+export function CheckmarkSize(width: number, height: number): TemplateResult;
+export function CheckmarkSize(size: number): TemplateResult;
+export function CheckmarkSize(): TemplateResult;
+export function CheckmarkSize(width: number = 24, height: number = width) {
+    return html`
+        <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/>
+        </svg>`
+}
--- shared/components/icons/lockClosed/lockClosed.ts
+++ shared/components/icons/lockClosed/lockClosed.ts
@@ -1,7 +1,14 @@
-import { html } from "lit-html";
+import { html, TemplateResult } from "lit-html";
 
-export const LockClosed = html`
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-	<path d="M0 0h24v24H0z" fill="none"/>
-	<path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"/>
-</svg>`;
+export const LockClosed = LockClosedSize();
+
+export function LockClosedSize(width: number, height: number): TemplateResult;
+export function LockClosedSize(size: number): TemplateResult;
+export function LockClosedSize(): TemplateResult;
+export function LockClosedSize(width: number = 24, height: number = width) {
+	return html`
+		<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 24 24">
+			<path d="M0 0h24v24H0z" fill="none"/>
+			<path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"/>
+		</svg>`
+}
