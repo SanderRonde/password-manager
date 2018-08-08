616032e7457b4a94d8d581f9804baa6e70a421db
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-08 22:04:17 +0200

    attempt to figure out why travis isn't running tests

--- server/test/tests/env.test.ts
+++ server/test/tests/env.test.ts
@@ -1,6 +1,8 @@
 import { isMongoConnected } from '../lib/db';
 import { test } from 'ava';
 
+console.log('Doing enviroment tests');
 test.serial('has set up mongo', async t => {
+	console.log('inside enviroment test');
 	t.true(await isMongoConnected())
 });
