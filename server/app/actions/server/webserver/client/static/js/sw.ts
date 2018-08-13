2b6e2124eaedefb0457d6f6ac6952ebfc8167b16
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-13 19:33:13 +0200

    add code for registering a serviceworker

--- server/app/actions/server/webserver/client/static/js/sw.ts
+++ server/app/actions/server/webserver/client/static/js/sw.ts
@@ -1,5 +1,13 @@
+import { TypedNavigator } from "../serviceworker";
+
+declare const navigator: TypedNavigator;
+
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker.register('/serviceworker.js');
+	});
+
+	navigator.serviceWorker.controller.postMessage({
+		type: 'checkVersions'
 	});
 }
