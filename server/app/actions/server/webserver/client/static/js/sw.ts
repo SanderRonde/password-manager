a36a7c17506d6d6d53b83a6f674c70b29ee29d08
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-13 22:36:32 +0200

    register as module

--- server/app/actions/server/webserver/client/static/js/sw.ts
+++ server/app/actions/server/webserver/client/static/js/sw.ts
@@ -6,7 +6,8 @@ declare const navigator: TypedNavigator;
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker.register('/serviceworker.js', {
-			scope: '/'
+			scope: '/',
+			type: 'module'
 		});
 	});
 
