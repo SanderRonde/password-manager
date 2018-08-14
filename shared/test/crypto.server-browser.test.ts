2657d4d9d276b890db5ddb3da2986f6a3ca72097
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-15 00:32:16 +0200

    remove promise

--- shared/test/crypto.server-browser.test.ts
+++ shared/test/crypto.server-browser.test.ts
@@ -58,14 +58,3 @@ test('data hybrid encrypted by the browser can be decrypted by the server', t =>
 	const decrypted = serverCrypto.hybdridDecrypt(encrypted, privateKey);
 	t.is(decrypted, input, 'decrypted value is the same as input');
 });
-
-function somePromise() {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve();
-		}, 15000);
-	});
-}
-test('something async', async _ => {
-	await somePromise();
-});
