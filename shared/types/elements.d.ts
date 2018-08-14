8e4fe628b81331e00e393701d8bc3d58a6368569
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-14 23:01:19 +0200

    add paper-toast element

--- shared/types/elements.d.ts
+++ shared/types/elements.d.ts
@@ -5,10 +5,12 @@ import { MaterialInput } from "../components/util/material-input/material-input"
 import { IconButton } from "../components/util/icon-button/icon-button";
 import { Login } from "../components/entrypoints/web/login/login";
 import { ThemeSelector } from "../components/util/theme-selector/theme-selector";
+import { PaperToast } from "../components/util/paper-toast/paper-toast";
 
 declare global {
 	type HTMLLoginElement = Login;
 	type HTMLIconButtonElement = IconButton;
+	type HTMLPaperToastElement = PaperToast;
 	type HTMLMaterialInputElement = MaterialInput;
 	type HTMLThemeSelectorElement = ThemeSelector;
 	type HTMLAnimatedButtonElement = AnimatedButton;
