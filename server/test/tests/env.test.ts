6f1a3fdccbc52368a6399b429d921858f9554499
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-15 17:44:53 +0200

    add timeout

--- server/test/tests/env.test.ts
+++ server/test/tests/env.test.ts
@@ -1,10 +1,11 @@
 import { isMongoConnected } from '../lib/db';
+import { mochaTest } from '../lib/util';
 import { assert } from 'chai';
 
 export function enviromentTests() {
 	describe('Enviroment', () => {
-		it('has set up mongo', async () => {
+		it('has set up mongo', mochaTest(150, async () => {
 			assert.isTrue(await isMongoConnected())
-		});
+		}));
 	});
 }
