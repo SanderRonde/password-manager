4d2a44713de8ecc6b1dd6511c643d136d7c2fec4
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-15 02:39:04 +0200

    remove .serial

--- server/test/tests/env.test.ts
+++ server/test/tests/env.test.ts
@@ -2,7 +2,7 @@ import { isMongoConnected } from '../lib/db';
 import { test } from 'ava';
 
 console.log('Doing enviroment tests');
-test.serial('has set up mongo', async t => {
+test('has set up mongo', async t => {
 	console.log('inside enviroment test');
 	t.true(await isMongoConnected())
 });
