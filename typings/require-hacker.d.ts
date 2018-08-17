16a4eb5a770f64e1edd3dc224d6f863cbac1b5f0
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-17 20:00:30 +0200

    update require-hacker defs

--- typings/require-hacker.d.ts
+++ typings/require-hacker.d.ts
@@ -1,10 +1,16 @@
 declare module "require-hacker" {
-	export function hook(extension: string, callback: (path: string) => string): void;
+	export function hook(extension: string, callback: (path: string) => string): {
+		unmount(): void;
+	};
 	export function global_hook(name: String, callback: (path: string, module: any) => {
 		source: string;
 		path: string;
-	}): void;
-	export function resolver(callback: (path: string, module: any) => string|void): void;
+	}): {
+		unmount(): void;
+	};
+	export function resolver(callback: (path: string, module: any) => string|void): {
+		unmount(): void;
+	};
 	export function to_javascript_module(anything: any): string;
 	export function resolve(path: string, module: any): string;
 }
