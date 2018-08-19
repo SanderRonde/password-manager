5c43187a2d868caade550ab284ae5c92650e986b
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-19 03:28:44 +0200

    don't print logs

--- server/test/unit/tests/cli/version.test.ts
+++ server/test/unit/tests/cli/version.test.ts
@@ -4,10 +4,7 @@ import { ProcRunner } from '../../lib/procrunner';
 export function cliVersionTest() {
 	describe('Version', () => {
 		it('display the version when calling it with -v', async () => {
-			const proc = new ProcRunner(['-v'], {
-				printifnomatch: true,
-				printlogs: true
-			});
+			const proc = new ProcRunner(['-v']);
 			proc.expectWrite(VERSION);
 			proc.expectExit(0);
 			await proc.run();
