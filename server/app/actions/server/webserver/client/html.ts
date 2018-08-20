b68d9a2c0a96247553e4d2ef70a652d9f2670c57
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-20 18:47:54 +0200

    allow fetches to any website

--- server/app/actions/server/webserver/client/html.ts
+++ server/app/actions/server/webserver/client/html.ts
@@ -42,7 +42,7 @@ export async function preAppHTML({
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
-		${conditionalString(`<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src 'self'; script-src 'self'; style-src 'unsafe-inline' 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; connect-src 'self'">`,
+		${conditionalString(`<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src 'self'; script-src 'self'; style-src 'unsafe-inline' 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; connect-src *">`,
 			!development)}
 		<meta name="theme-color" content="#455A64">
 		<meta name="description" content="Your password manager dashboard">
