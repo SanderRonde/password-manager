2223213b43b3e0cf9fa323cb7f938040077e9046
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-20 21:38:43 +0200

    load settings from cypress.json

--- test/ui/test-cypress.ts
+++ test/ui/test-cypress.ts
@@ -2,6 +2,7 @@ import { runCypressServer } from './run-cypress-server';
 import { UI_TEST_PORT } from './lib/ui-test-const';
 import { createServer } from 'http-server';
 import * as cypress from 'cypress';
+import * as fs from 'fs-extra';
 import * as path from 'path';
 
 const httpServer = createServer({
@@ -11,6 +12,10 @@ httpServer.listen(UI_TEST_PORT, async () => {
 	await runCypressServer();	
 
 	cypress.run({
+		...JSON.parse(await fs.readFile(path.join(__dirname, '../../',
+		'cypress.json'), {
+			encoding: 'utf8'
+		})),
 		record: process.argv.indexOf('--no-record') ? false : true
 	}).then((results) => {
 		httpServer.close();
