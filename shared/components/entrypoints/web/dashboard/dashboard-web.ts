69f7e5a2fb31743de2667f6fbd2b295a09bfd97d
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-21 23:24:47 +0200

    implement getting of password data

--- shared/components/entrypoints/web/dashboard/dashboard-web.ts
+++ shared/components/entrypoints/web/dashboard/dashboard-web.ts
@@ -1,10 +1,108 @@
+import { ERRS, Hashed, Padded, MasterPasswordDecryptionpadding } from '../../../../types/crypto';
+import { decryptWithPrivateKey, hash, pad } from '../../../../lib/browser-crypto';
+import { MaterialInput } from '../../../util/material-input/material-input';
+import { GlobalControllerData } from '../../global/global-controller';
+import { DashboardHTML } from '../../base/dashboard/dashboard.html';
+import { PaperToast } from '../../../util/paper-toast/paper-toast';
+import { DashboardCSS } from '../../base/dashboard/dashboard.css';
+import { doClientAPIRequest } from '../../../../lib/apirequests';
+import { MasterPassword } from '../../../../types/db-types';
 import { Dashboard } from '../../base/dashboard/dashboard';
 import { config } from '../../../../lib/webcomponent-util';
-import { html } from 'lit-html';
+import { Remove } from '../../../../types/serviceworker';
 
 @config({
 	is: 'dashboard-page',
-	css: null,
-	html: () => html`<div>Dashboard</div>`
+	css: DashboardCSS,
+	html: DashboardHTML,
+	dependencies: [
+		MaterialInput,
+		PaperToast
+	]
 })
-export class DashboardWeb extends Dashboard { }
+export class DashboardWeb extends Dashboard { 
+	private _data!: Remove<GlobalControllerData['loginData'], 'password' & {
+		decrypt_hash: Hashed<Padded<MasterPassword, MasterPasswordDecryptionpadding>>;	
+	}>;
+
+	private _failNoCredentials(message: string) {
+		PaperToast.create({
+			content: message + ', redirecting to login page...',
+			buttons: [PaperToast.BUTTONS.HIDE],
+			duration: 10000
+		});
+		this.getRoot().changePage('login');
+	}	
+
+	private _initData() {
+		const data = this.getRoot().getData('loginData');
+		if (!data) {
+			return null;
+		}
+		
+		const decryptHashed = hash(pad(data.password, 'masterpwdecrypt'));
+		const verifyHashed = hash(pad(data.password, 'masterpwverify'));
+		return {
+			verifyHashed,
+			newData: {...data, ...{
+				password: undefined,
+				decrypt_hash: decryptHashed
+			}}
+		}
+	}
+
+	protected async _getPasswordMeta() {
+		const data = this._initData();
+		if (!data) {
+			//No global data, redirect to login
+			this._failNoCredentials('No credentials');
+			return null;
+		}
+		const { newData, verifyHashed } = data;
+		this._data = newData as any;
+
+		if (this._globalProperties.password_meta) {
+			const decrypted = decryptWithPrivateKey(
+				this._globalProperties.password_meta,
+				this._data.private_key);
+			if (decrypted === ERRS.INVALID_DECRYPT) {
+				this._failNoCredentials('Failed to decrypt data');
+				return null;
+			}
+			try {
+				return JSON.parse(decrypted);
+			} catch(e) {
+				this._failNoCredentials(`Failed to parse data`);
+				return null;
+			}
+		}
+
+		const res = await doClientAPIRequest({
+			publicKey: this._data.server_public_key
+		}, '/api/password/allmeta', {
+			instance_id: this._data.instance_id
+		}, {
+			count: 0,
+			token: this._data.login_auth,
+			password_hash: verifyHashed
+		});
+		if (res.success === false) {
+			this._failNoCredentials(`Failed to get passwords "${res.ERR}"`);
+			return null;
+		}
+
+		const decrypted = decryptWithPrivateKey(
+			res.data.encrypted,
+			this._data.private_key);
+		if (decrypted === ERRS.INVALID_DECRYPT) {
+			this._failNoCredentials('Failed to decrypt data');
+			return null;
+		}
+		try {
+			return JSON.parse(decrypted);
+		} catch(e) {
+			this._failNoCredentials(`Failed to parse data`);
+			return null;
+		}
+	}
+}
