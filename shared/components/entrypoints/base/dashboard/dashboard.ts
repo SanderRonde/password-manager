367ad86499ffb8fec8441ef63725e730b0386d80
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-21 23:08:15 +0200

    add main dashboard prop

--- shared/components/entrypoints/base/dashboard/dashboard.ts
+++ shared/components/entrypoints/base/dashboard/dashboard.ts
@@ -1,3 +1,27 @@
 import { ConfigurableWebComponent } from "../../../../lib/webcomponents";
+import { defineProps, JSONType } from '../../../../lib/webcomponent-util';
+import { PublicKeyDecrypted } from '../../../../types/db-types';
+import { APISuccessfulReturns } from '../../../../types/api';
 
-export abstract class Dashboard extends ConfigurableWebComponent { }
+export type MetaPasswords = PublicKeyDecrypted<APISuccessfulReturns['/api/password/allmeta']['encrypted']>;
+export abstract class Dashboard extends ConfigurableWebComponent { 
+	props = defineProps(this, {
+		priv: {
+			metaPasswords: {
+				type: JSONType<MetaPasswords>(),
+				value: []
+			}
+		}
+	});
+
+	protected abstract _getPasswordMeta(): Promise<MetaPasswords|null>|MetaPasswords|null;
+
+	async mounted() {
+		const pwMeta = await this._getPasswordMeta();
+		if (!pwMeta) {
+			//Redirecting to /login, just let this go
+			return;
+		}
+		this.props.metaPasswords = pwMeta;
+	}
+}
