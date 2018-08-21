c064b98dd2d6d8bd74e9bb2a691ebf973b513ece
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-21 23:07:31 +0200

    create initial dashboard css page

---
+++ shared/components/entrypoints/base/dashboard/dashboard.css.ts
@@ -0,0 +1,9 @@
+import { Theme } from '../../../../types/shared-types';
+import { Dashboard } from './dashboard';
+import { html } from "lit-html";
+
+export function DashboardCSS(this: Dashboard, _theme: Theme, _props: Dashboard['props']) {
+	return html`<style>
+		
+	</style>`
+}
