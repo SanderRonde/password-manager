a96609ef7ab9dd3fb0ba916e29bf5debd045e988
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-22 03:25:56 +0200

    remove test/ compilation from dev env setup

--- scripts/start-dev-env.bat
+++ scripts/start-dev-env.bat
@@ -4,7 +4,6 @@ SET script_dir=%~dp0
 cd %script_dir%/../
 start cmd /k tsc -p server/tsconfig.json -w
 start cmd /k tsc -p shared/tsconfig.json -w
-start cmd /k tsc -p test/tsconfig.json -w
 start cmd /k tsc -p server/app/actions/server/webserver/client/src/tsconfig.json -w
 mkdir "%script_dir%..\database\"
 start cmd /k mongod --dbpath=%script_dir%..\database\
--- scripts/start-dev-env.sh
+++ scripts/start-dev-env.sh
@@ -6,7 +6,6 @@ script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $script_path/../
 terminal -e tsc -p server/tsconfig.json -w
 terminal -e tsc -p shared/tsconfig.json -w
-terminal -e tsc -p test/tsconfig.json -w
 terminal -e tsc -p server/app/actions/server/webserver/client/src/tsconfig.json -w
 mkdir "$script_path/../database/"
 terminal -e mongod --dbpath=$script_path/../database/
