08fe830ac8a143ca34e34e6b286ea8d11f87d97a
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-22 23:29:45 +0200

    define global controller

--- server/app/actions/server/webserver/client/src/entrypoints/dashboard/dashboard-page.ts
+++ server/app/actions/server/webserver/client/src/entrypoints/dashboard/dashboard-page.ts
@@ -1,5 +1,7 @@
 import { registerServiceWorker } from "../../../static/js/sw";
 import { DashboardWeb } from "../../../../../../../../../shared/components/entrypoints/web/dashboard/dashboard-web";
+import { GlobalController } from "../../../../../../../../../shared/components/entrypoints/global/global-controller";
 
+GlobalController.define();
 DashboardWeb.define();
 registerServiceWorker();
