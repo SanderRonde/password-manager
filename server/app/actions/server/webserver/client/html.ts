f631e3515dd687add838ef34bbcad33ea6fef57d
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-27 02:41:49 +0200

    add web manifest (icons still incomplete)

--- server/app/actions/server/webserver/client/html.ts
+++ server/app/actions/server/webserver/client/html.ts
@@ -42,8 +42,9 @@ export async function preAppHTML({
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
-		${conditionalString(`<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src 'self'; script-src 'self'; style-src 'unsafe-inline' 'self'; connect-src *">`,
+		${conditionalString(`<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src 'self'; script-src 'self'; style-src 'unsafe-inline' 'self'; connect-src *; manifest-src 'self'">`,
 			!development)}
+		<link rel="manifest" href="/about/manifest.json">
 		<meta name="theme-color" content="#455A64">
 		<meta name="description" content="Your password manager dashboard">
 		<title>${title}</title>
