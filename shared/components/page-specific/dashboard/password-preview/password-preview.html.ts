f9fc396ab6de93dbefbe1639f612e924119893ce
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-08-31 18:46:46 +0200

    create basic pasword preview layout

--- shared/components/page-specific/dashboard/password-preview/password-preview.html.ts
+++ shared/components/page-specific/dashboard/password-preview/password-preview.html.ts
@@ -2,20 +2,39 @@ import { TemplateFn, CHANGE_TYPE } from '../../../../lib/webcomponents';
 import { PasswordPreview } from './password-preview';
 import { html } from 'lit-html';
 
+function prefixWithWWW(url: string) {
+	if (url.startsWith('www.')) {
+		return url;
+	}
+	return `www.${url}`;
+}
 export const PasswordPreviewHTML = new TemplateFn<PasswordPreview>((props) => {
 	return html`
 		<md-card>
-			<div>ID: ${props.id}</div>
-			<div>
+			<div id="websites">
 				${(props.websites || []).map((website) => {
 					return html`
-						<div>host: ${website.host}</div>
-						<div>exact: ${website.exact}</div>
-						<div>favicon: ${website.favicon}</div>
+						<div class="website">
+							<div class="icon">
+								${website.favicon ? html`
+									<img src="${website.favicon}">
+								` : html`
+									No icon
+								`}
+							</div>
+							<div class="urls">
+								<div class="url" title="${website.exact}">
+									${prefixWithWWW(website.host)}
+								</div>
+								<div class="username">${props.username}</div>
+							</div>
+						</div>
 					`
 				})}
 			</div>
-			<div>2FA enabled: ${props.twofactor_enabled.toString()}</div>
+			<div id="pointer">
+				<div>2FA enabled: ${props.twofactor_enabled.toString()}</div>
+			</div>
 		</md-card>
 	`
 }, CHANGE_TYPE.PROP);
