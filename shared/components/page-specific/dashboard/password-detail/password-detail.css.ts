7efa91a5b408bfb24522134e4c3a96db786d5866
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-09-02 22:33:16 +0200

    add missing imports

--- shared/components/page-specific/dashboard/password-detail/password-detail.css.ts
+++ shared/components/page-specific/dashboard/password-detail/password-detail.css.ts
@@ -1,4 +1,5 @@
-import { TemplateFn, CHANGE_TYPE } from '../../../lib/webcomponents';
+import { TemplateFn, CHANGE_TYPE } from '../../../../lib/webcomponents';
+import { PasswordDetail } from './password-detail';
 import { html } from 'lit-html';
 
 export const PasswordDetailCSS = new TemplateFn<PasswordDetail>((_props) => {
--- shared/components/page-specific/dashboard/password-detail/password-detail.html.ts
+++ shared/components/page-specific/dashboard/password-detail/password-detail.html.ts
@@ -1,8 +1,13 @@
-import { TemplateFn, CHANGE_TYPE } from '../../../lib/webcomponents';
+import { TemplateFn, CHANGE_TYPE } from '../../../../lib/webcomponents';
+import { PasswordDetail } from './password-detail';
 import { html } from 'lit-html';
 
 export const PasswordDetailHTML = new TemplateFn<PasswordDetail>((_props) => {
 	return html`
-		<div></div>
+		<div>
+			<md-card id="passwordFocus" level="3">
+				Select something
+			</md-card>
+		</div>
 	`
 }, CHANGE_TYPE.PROP);
