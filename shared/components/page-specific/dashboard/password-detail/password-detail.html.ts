4a3923df31abd22fdc225b9c9da68041749ba828
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-09-08 02:38:48 +0200

    mark variable as unused

--- shared/components/page-specific/dashboard/password-detail/password-detail.html.ts
+++ shared/components/page-specific/dashboard/password-detail/password-detail.html.ts
@@ -3,7 +3,7 @@ import { TemplateFn, CHANGE_TYPE } from '../../../../lib/webcomponents';
 import { PasswordDetail } from './password-detail';
 import { html } from 'lit-html';
 
-export const PasswordDetailHTML = new TemplateFn<PasswordDetail>((props) => {
+export const PasswordDetailHTML = new TemplateFn<PasswordDetail>((_props) => {
 	return html`
 		<div>
 			<md-card level="3" 
