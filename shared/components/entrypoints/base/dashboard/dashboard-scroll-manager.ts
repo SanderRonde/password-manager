46c109c4dc2c1f85845a695e53373b54486ad71b
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-09-10 23:15:30 +0200

    don't rely solely on state for toggling scroll

--- shared/components/entrypoints/base/dashboard/dashboard-scroll-manager.ts
+++ shared/components/entrypoints/base/dashboard/dashboard-scroll-manager.ts
@@ -12,29 +12,9 @@ export abstract class DashboardScrollManager extends ConfigurableWebComponent<Da
 		return 'IntersectionObserver' in window;
 	}
 
-	private _detailViewWasScrolledUp: boolean = false;
-	private _isInitialDetailViewIntersectCall: boolean = true;
 	@bindToClass
-	private _detailViewIntersect() {
-		if (this._isInitialDetailViewIntersectCall) {
-			this._isInitialDetailViewIntersectCall = false;
-			return;
-		}
-		const isScrolledUp = !this._detailViewWasScrolledUp;
-		this._detailViewWasScrolledUp = isScrolledUp;
-
-		if (isScrolledUp) {
-			this._passwordFocusIsFixed = true;
-			this.$.passwordDetail.classList.add('fixed');
-		}
-	}
-	
-	@bindToClass
-	private _listIntersect(entries: IntersectionObserverEntry[]) {
-		if (entries[0].intersectionRatio) {
-			this._passwordFocusIsFixed = false;
-			this.$.passwordDetail.classList.remove('fixed');
-		}
+	private _listIntersect() {
+		this._scroll();
 	}
 
 	private _observed: {
@@ -42,10 +22,6 @@ export abstract class DashboardScrollManager extends ConfigurableWebComponent<Da
 		list: HTMLElement;
 	}|null = null;
 	private _observers = this._intersectionObserverSupported ? {
-		detailView: new IntersectionObserver(this._detailViewIntersect, {
-			root: null,
-			rootMargin: `-${PW_VIEW_SCROLL}px 0px 0px 0px`
-		}),
 		list: new IntersectionObserver(this._listIntersect, {
 			root: null,
 			rootMargin: `-${PW_VIEW_SCROLL}px 0px 0px 0px`
@@ -55,7 +31,7 @@ export abstract class DashboardScrollManager extends ConfigurableWebComponent<Da
 	private _currentAnimationFrame: number|null = null;
 	private _passwordFocusIsFixed: boolean = false;
 	private _scroll() {
-		if (this._passwordFocusIsFixed && document.documentElement.scrollTop <= MAX_DOC_SCROLL) {
+		if (this._passwordFocusIsFixed || document.documentElement.scrollTop <= MAX_DOC_SCROLL) {
 			this._passwordFocusIsFixed = false;
 			this.$.passwordDetail.classList.remove('fixed');
 		} else if (!this._passwordFocusIsFixed && 
@@ -84,23 +60,6 @@ export abstract class DashboardScrollManager extends ConfigurableWebComponent<Da
 	}
 
 	postRender() {
-		if (isNewElement(this.$.passwordDetailTop)) {
-			this._detailViewWasScrolledUp = this.$.passwordDetailTop.getBoundingClientRect().top < 0;
-			if (this._intersectionObserverSupported) {
-				if (this._observed && this._observed.detailView) {
-					this._observers!.detailView.unobserve(this._observed.detailView);
-				}
-				this._observers!.detailView.observe(this.$.passwordDetailTop);
-				this._observed = this._observed || {} as Partial<{
-					detailView: HTMLElement;
-					list: HTMLElement;
-				}> as {
-					detailView: HTMLElement;
-					list: HTMLElement;
-				};
-				this._observed.detailView = this.$.passwordDetailTop;
-			}
-		}
 		if (isNewElement(this.$.passwordTop)) {
 			if (this._intersectionObserverSupported) {
 				if (this._observed && this._observed.list) {
