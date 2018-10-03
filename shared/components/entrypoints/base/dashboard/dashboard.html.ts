01e0f5ef508fe5b28633dd24fbd7957c0f33fcf8
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-10-03 03:33:19 +0200

    add template while loading password meta

--- shared/components/entrypoints/base/dashboard/dashboard.html.ts
+++ shared/components/entrypoints/base/dashboard/dashboard.html.ts
@@ -1,4 +1,5 @@
 import { TemplateFn, CHANGE_TYPE } from '../../../../lib/webcomponents';
+import { joinTemplates } from '../../../../lib/webcomponent-util';
 import { Search } from '../../../icons/search/search';
 import { CustomDashboardCSS } from './dashboard.css';
 import { Dashboard } from './dashboard';
@@ -20,32 +21,65 @@ export const DashboardHTML = new TemplateFn<Dashboard>(function (props, _theme, 
 				<div id="pageContainer">
 					<div id="passwordList">
 						<div id="passwordTop"></div>
-						${props.metaPasswords && props.metaPasswords.length === 0 ?
-							html`<md-card id="noPasswords" level="3">
+						${props.metaPasswords ? '' : html`
+							<infinite-list custom-css="${joinTemplates(
+								CustomDashboardCSS.infiniteList,
+								CustomDashboardCSS.templateList
+							)}" 
+								window id="placeholderList"
+								item-size="${() => this.getItemSize(null, {
+									isMin: true
+								})}"
+								ref="${this}"
+								data="${this.getPlaceholderList()}"
+							>
+								<template slot="template">
+									<md-card level="3"
+										padding-vertical="0"
+										padding-horizontal="0"
+									>
+										<div class="templateContainer">
+											<div class="templateIcon"></div>
+											<div class="templateWebsite">
+												<div class="templateLine"></div>
+												<div class="templateLine"></div>
+											</div>
+											<div class="templateArrow">
+												<div class="__hollow_arrow"></div>
+											</div>
+										</div>
+									</md-card>
+								</template>
+							</infinite-list>
+						`}
+						${!(props.metaPasswords && props.metaPasswords.length === 0) ? '' : html`
+							<md-card id="noPasswords" level="3">
 								No passwords, click the add button to add some
-							</md-card>` : html`
-								<infinite-list custom-css="${CustomDashboardCSS.infiniteList}" 
-									window id="infiniteList" data-name="password" 
-									item-size="${this.getItemSize}"
-									ref="${this}"
-									default-item-data="${{
-										selected: false
-									}}"
-									data="${props.metaPasswords || []}"
-								>
-									<template slot="template">
-										<password-preview id="password.id"
-											websites="password.websites"
-											username="password.username"
-											twofactor_enabled="password.twofactor_enabled"
-											u2f_enabled="password.u2f_enabled"
-											selected="password_data.selected"
-											index="_index"
-											ref="_this"
-										></password-preview>
-									</template>
-								</infinite-list>`
-							}
+							</md-card>
+						`}
+						${!(props.metaPasswords && props.metaPasswords.length) ? '' : html`
+							<infinite-list custom-css="${CustomDashboardCSS.infiniteList}" 
+								window id="infiniteList" data-name="password" 
+								item-size="${this.getItemSize}"
+								ref="${this}"
+								default-item-data="${{
+									selected: false
+								}}"
+								data="${props.metaPasswords || []}"
+							>
+								<template slot="template">
+									<password-preview id="password.id"
+										websites="password.websites"
+										username="password.username"
+										twofactor_enabled="password.twofactor_enabled"
+										u2f_enabled="password.u2f_enabled"
+										selected="password_data.selected"
+										index="_index"
+										ref="_this"
+									></password-preview>
+								</template>
+							</infinite-list>
+						`}
 					</div>
 					<div id="passwordDetail">
 						<div id="passwordDetailTop"></div>
