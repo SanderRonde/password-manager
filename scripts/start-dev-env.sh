22d68a43e940312028c41021fe2bb653dc92551a
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-12-18 16:02:00 +0100

    use default terminal

--- scripts/start-dev-env.sh
+++ scripts/start-dev-env.sh
@@ -3,11 +3,16 @@
 current_dir=`echo $PWD`
 script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+TERM=$TERMINAL
+if [ -z $TERM ]; then
+	TERM="terminal"
+fi
+
 cd $script_path/../
-terminal -e tsc -p server/tsconfig.json -w
-terminal -e tsc -p shared/tsconfig.json -w
-terminal -e tsc -p server/app/actions/server/webserver/client/tsconfig.json -w
-terminal -e gulp watch
+$TERM -e "bash -c \"tsc -p server/tsconfig.json -w\""
+$TERM -e "bash -c \"tsc -p shared/tsconfig.json -w\""
+$TERM -e "bash -c \"tsc -p server/app/actions/server/webserver/client/tsconfig.json -w\""
+$TERM -e "bash -c \"gulp watch\""
 mkdir "$script_path/../database/"
-terminal -e mongod --dbpath=$script_path/../database/
+$TERM -e "bash -c \"mongod --dbpath=$script_path/../database/\""
 nodemon
