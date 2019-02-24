f209aede8815d2fdaae3ecc99c8cf4b3672def8b
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-02-24 23:24:28 +0100

    use cjs themes

--- test/ui/integration/components/animated-button/animated-button.bundled.ts
+++ test/ui/integration/components/animated-button/animated-button.bundled.ts
@@ -1,6 +1,6 @@
 import { AnimatedButton } from '../../../../../shared/components/util/animated-button/animated-button';
 import { WebComponentThemeManger } from '../../../../../shared/lib/webcomponents/theme-manager';
-import { theme } from '../../../../../shared/components/theming/theme/theme.es';
+import { theme } from '../../../../../shared/components/theming/theme/theme.cjs';
 
 WebComponentThemeManger.setTheme(theme, 'light');
 AnimatedButton.define();
